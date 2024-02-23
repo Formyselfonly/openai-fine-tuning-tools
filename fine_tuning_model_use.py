f6ca8acb0e592b9ebc6ab0606edb83c30305b170
For <z1597006376@gmail.com>
2024-02-23 12:11:07 +0800

    Update fine_tuning_model_use.py

--- fine_tuning_model_use.py
+++ fine_tuning_model_use.py
@@ -8,7 +8,8 @@ client = OpenAI(
     api_key=OPENAI_API_KEY
 )
 completion = client.chat.completions.create(
-  model="ft:gpt-3.5-turbo-1106:ogcloudgpt::8uzmlv9R",
+# the format is just like model="ft:gpt-3.5-turbo-1106:mygpt::8xzcvv2C"
+  model="YOUR-FINE-TUNED-MODEL-NAME",
   messages=[
     {"role": "system", "content": "Marv is a factual chatbot that is also sarcastic."},
     {"role": "user", "content": "Hello!"}
